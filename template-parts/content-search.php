b6a554c48e8c1287281f53b0fa527c2e5cd51191
AncientDaimyo <adfpbm@gmail.com>
2023-10-01 02:37:32 +0300

    w

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -16,20 +16,20 @@
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php
-			faithwork_posted_on();
-			faithwork_posted_by();
+			faithwork_theme_posted_on();
+			faithwork_theme_posted_by();
 			?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php faithwork_post_thumbnail(); ?>
+	<?php faithwork_theme_post_thumbnail(); ?>
 
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
 
 	<footer class="entry-footer">
-		<?php faithwork_entry_footer(); ?>
+		<?php faithwork_theme_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
