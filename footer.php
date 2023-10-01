a6e4607eaffd5640566f5af35a9c2ea572ceeb74
AncientDaimyo <adfpbm@gmail.com>
2023-10-01 03:44:54 +0300

    wdwdw

--- footer.php
+++ footer.php
@@ -12,19 +12,12 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'faithwork-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'faithwork-theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'faithwork-theme' ), 'faithwork-theme', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
+		<div class="footer-top">
+			<img class="footer-image" src="~/faithwork-theme/pic/Faithwork_Name_Logo.png">
+		</div>
+		<div class="footer-bottom">
+
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
