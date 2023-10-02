387ae87c89fe09f03fa29203c70c6428051cf9ed
AncientDaimyo <adfpbm@gmail.com>
2023-10-03 00:53:45 +0300

    add marquee script

--- main-page-template.php
+++ main-page-template.php
@@ -37,6 +37,30 @@ get_header();
 
     // endif;
     ?>
-    <marquee behavior="scroll" direction="right" scrollamount="18" scrolldelay="0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</marquee>
+    <!-- <marquee behavior="scroll" direction="right" scrollamount="18" scrolldelay="0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</marquee> -->
+    <script type="text/javascript">
+        function tekst(msg, ctrlwidth) {
+            msg = " " + msg
+            newmsg = msg
+            while (newmsg.length < ctrlwidth) {
+                newmsg += msg
+            }
+            document.write('<form name="Tekst">')
+            document.write('<input name="tekst" style="border:0;font-weight:bold;" value= "' + newmsg + '" size= ' + ctrlwidth + ' />')
+            document.write('</form>')
+            prokrutka()
+        }
+
+        function prokrutka() {
+            NowMsg = document.Tekst.tekst.value
+            NowMsg = NowMsg.substring(1, NowMsg.length) + NowMsg.substring(0, 1)
+            document.Tekst.tekst.value = NowMsg
+            bannerid = setTimeout("prokrutka()", 56)
+        }
+        //
+    </script>
+    <script type="text/javascript">
+        tekst("Бегущая в JavaScript", 50)
+    </script>
 </main>
 <?php get_footer(); ?>
