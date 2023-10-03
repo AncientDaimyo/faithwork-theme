d6b0f8b4d5ded9914699afad2a9eb49c7155ef83
linnerrion <arcticshock15@gmail.com>
2023-10-03 15:08:50 +0300

    ыщаьыа

--- main-page-template.php
+++ main-page-template.php
@@ -10,7 +10,7 @@ get_header();
 <main id="primary" class="site-main custom">
     <section class="main-block first">
         <!-- <img src="wp-content/themes/faithwork-theme/pic/bg_image.png"> -->
-        <video loop="loop" autoplay="muted">
+        <video loop="loop" preload="auto" autoplay="autoplay" muted="">
             <source src="wp-content/themes/faithwork-theme/Video/IMG_0852.mp4">
         </video>
     </section>
