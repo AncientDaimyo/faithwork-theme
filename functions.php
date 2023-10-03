0e5d951042462566ddd42d118e224b9b4cfe3f44
AncientDaimyo <adfpbm@gmail.com>
2023-10-03 17:14:22 +0300

    w

--- functions.php
+++ functions.php
@@ -151,6 +151,7 @@ function faithwork_theme_scripts() {
 	wp_enqueue_script( 'faithwork-theme-menu', get_template_directory_uri() . '/js/menu.js', array(), 1.0, true );
 	wp_enqueue_script( 'faithwork-theme-scroll', get_template_directory_uri() . '/js/scroll.js', array(), 1.0, true );
 	wp_enqueue_script( 'faithwork-theme-marquee', get_template_directory_uri() . '/js/begushyaya.js', array(), 1.0, true );
+	wp_enqueue_script( 'faithwork-theme-slider', get_template_directory_uri() . '/js/slider.js', array(), 1.0, true );
 	wp_enqueue_script( 'jquery');
 	wp_enqueue_script( 'jquery-marquee', 'https://cdn.jsdelivr.net/jquery.marquee/1.4.0/jquery.marquee.min.js');
 	
