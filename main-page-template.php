2f6264c82320d0d6358819fd794636cf2aae45f9
linnerrion <arcticshock15@gmail.com>
2023-10-03 15:29:12 +0300

    Изменил видео и размеры дубль джва

--- main-page-template.php
+++ main-page-template.php
@@ -10,8 +10,8 @@ get_header();
 <main id="primary" class="site-main custom">
     <section class="main-block first">
         <!-- <img src="wp-content/themes/faithwork-theme/pic/bg_image.png"> -->
-        <video loop="loop" preload="auto" autoplay muted>
-            <source src="wp-content/themes/faithwork-theme/Video/IMG_0852.mp4">
+        <video class="video-main-autoplay" loop="loop" preload="auto" autoplay muted>
+            <source src="wp-content/themes/faithwork-theme/Video/IMG_0864.mp4">
         </video>
     </section>
     <section class="main-block second">
