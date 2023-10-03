bb30cd1725ca579874464430cca769fa632225c5
AncientDaimyo <adfpbm@gmail.com>
2023-10-03 16:50:52 +0300

    w

--- main-page-template.php
+++ main-page-template.php
@@ -47,8 +47,8 @@ get_header();
     <div class="marquee">
         <div class="marquee-wrapper">
             <?php
-            $marquee_text = '1';
-            for ($i = 1; $i <= 10; $i++) {
+            $marquee_text = 'Я ПОДЖИГАЛ ГОРОДА ПОКА ТЫ ОБИТАЛ';
+            for ($i = 1; $i <= 30; $i++) {
             ?>
                 <div class="marquee-content"><?php echo ($marquee_text); ?></div>
             <?php
