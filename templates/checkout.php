43b1b60f753e5e4b92c298b8e5b1fcbad8000ee8
linnerrion <arcticshock15@gmail.com>
2023-10-04 19:07:41 +0300

    Update checkout.php

--- templates/checkout.php
+++ templates/checkout.php
@@ -1,21 +1,28 @@
 <?php
+
 /**
  * @package faithwork-theme
  *
-*/
+ */
 /*
 Template Name: checkout
 */
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
-		<?php
-		get_template_part( 'template-parts/custom-content');
-		?>
+	<?php
 
-	</main><!-- #main -->
+	$args = array(
+		'delimiter' => '→' // меняем разделитель
+	);
+	woocommerce_breadcrumb($args);
+
+	get_template_part('template-parts/custom-content');
+	?>
+
+</main><!-- #main -->
 
 <?php
 get_footer("second");
