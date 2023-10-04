276c4b8f16e9f5bb5215401c16e1c9e4b1b30a5c
AncientDaimyo <adfpbm@gmail.com>
2023-10-04 15:04:40 +0300

    w

--- template-parts/slider.php
+++ template-parts/slider.php
@@ -11,7 +11,7 @@
 
 <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-<script type="text/javascript" src="slick/slick.min.js"></script>
+<script type="text/javascript" src="<? get_template_directory_uri() . 'slick/slick.min.js'?>"></script>
 
 <script type="text/javascript">
   jQuery(document).ready(function() {
