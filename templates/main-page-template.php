3e7a45d0f67aba667959fce9b65b61e94f42aae0
AncientDaimyo <adfpbm@gmail.com>
2023-10-04 14:19:30 +0300

    w

--- templates/main-page-template.php
+++ templates/main-page-template.php
@@ -18,33 +18,8 @@ get_header();
         <?php echo (get_theme_mod('marque_text')); ?>
     </section>
     <section class="main-block third">
-        <!-- <?php get_template_part('template-parts/slider');?> -->
+        <?php get_template_part('template-parts/slider');?>
     </section>
-    <?php
-    // if ( have_posts() ) :
-
-    // 	/* Start the Loop */
-    // 	while ( have_posts() ) :
-    // 		the_post();
-
-    // 		/*
-    // 		 * Include the Post-Type-specific template for the content.
-    // 		 * If you want to override this in a child theme, then include a file
-    // 		 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-    // 		 */
-    // 		get_template_part( 'template-parts/content', get_post_type() );
-
-    // 	endwhile;
-
-    // 	the_posts_navigation();
-
-    // else :
-
-    // 	get_template_part( 'template-parts/content', 'none' );
-
-    // endif;
-    ?>
-
 
     <div class="marquee">
         <div class="marquee-wrapper">
