5c5180014c747368362b32015f83008b22d57276
AncientDaimyo <adfpbm@gmail.com>
2023-10-04 15:11:28 +0300

    w

--- template-parts/slider.php
+++ template-parts/slider.php
@@ -1,6 +1,6 @@
 <title>My Now Amazing Webpage</title>
-<link rel="stylesheet" type="text/css" href="slick/slick.css" />
-<link rel="stylesheet" type="text/css" href="slick/slick-theme.css" />
+<link rel="stylesheet" type="text/css" href="<? get_template_directory_uri() . 'js/slick/slick.css'?>" />
+<link rel="stylesheet" type="text/css" href="<? get_template_directory_uri() . 'js/slick/slick-theme.css'?>" />
 
 
 <div class="slider">
@@ -11,4 +11,4 @@
 
 <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-<script type="text/javascript" src="<? get_template_directory_uri() . 'slick/slick.min.js'?>"></script>
+<script type="text/javascript" src="<? get_template_directory_uri() . 'js/slick/slick.min.js'?>"></script>
