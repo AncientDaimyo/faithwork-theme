3c09dcf99d89b407249f01a70999e2d8d93f4b82
AncientDaimyo <adfpbm@gmail.com>
2023-10-04 14:24:41 +0300

    w

--- template-parts/slider.php
+++ template-parts/slider.php
@@ -1,19 +1,19 @@
 <div class="slider">
   <ul>
     <li>
-      <ing class="slider-img" src="https://media.geeksforgeeks.org/wp-content/uploads/20230329113637/image1.png" alt="GFG Image 1">
+      <img class="slider-img" src="https://media.geeksforgeeks.org/wp-content/uploads/20230329113637/image1.png" alt="GFG Image 1">
       <div class="caption">
         Geeks for Geeks
       </div>
     </li>
     <li>
-      <ing class="slider-img" src="https://media.geeksforgeeks.org/wp-content/uploads/20230329113637/image2.jpeg" alt="GFG Image 2">
+      <img class="slider-img" src="https://media.geeksforgeeks.org/wp-content/uploads/20230329113637/image2.jpeg" alt="GFG Image 2">
       <div class="caption">
         Geeks for Geeks
       </div>
     </li>
     <li>
-      <ing class="slider-img" src="https://media.geeksforgeeks.org/wp-content/uploads/20230329113638/image3.jpeg" alt="GFG Image 3">
+      <img class="slider-img" src="https://media.geeksforgeeks.org/wp-content/uploads/20230329113638/image3.jpeg" alt="GFG Image 3">
       <div class="caption">
         Geeks for Geeks
       </div>
